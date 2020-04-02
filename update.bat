c4d88515aed7b77e36ca72b795ef43b56bfcf805
Roger Ye <shugaoye@yahoo.com>
2020-04-02 15:55:33 +0800

    Added update.bat.

---
+++ update.bat
@@ -0,0 +1,4 @@
+copy dist\main.js ..\PassXYZ.UI.Droid\Assets\main.js
+copy dist\main.js ..\PassXYZ.UI.iOS\Resources\main.js
+copy dist\main.js ..\PassXYZ.UI.Uwp\Assets\main.js
+copy dist\main.js ..\PassXYZ.UI.Editor\Uwp\Assets\main.js
